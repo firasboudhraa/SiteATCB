ce0631d46d6c0f425530aab4cad307d2942f5f89
firasboudhraa <fboudhraa2@gmail.com>
2024-11-02 10:50:25 +0100

    adding cards

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./globals.css"
 import Hero from "@/components/organisims/Hero/Hero"
+import Cards from "@/components/organisims/Cards/Cards"
 
 export default function Home() {
   return (
     <div>
       <Hero/>
+      <Cards/>
     </div>
   );
 }
